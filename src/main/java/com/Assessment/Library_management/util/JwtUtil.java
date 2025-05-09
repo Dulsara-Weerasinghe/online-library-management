4b4a7147359a85da879ba3794b2864444d873750
dulsara_w <dulsara_w@epiclanka.net>
2025-05-09 13:46:42 +0530

    new annotation added

--- src/main/java/com/Assessment/Library_management/util/JwtUtil.java
+++ src/main/java/com/Assessment/Library_management/util/JwtUtil.java
@@ -5,11 +5,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+@Component
 public class JwtUtil {
     @Value("${jwt.secret.key}")
     public static String secretKey;
